a8a8e4f7786e14b2c22b3a5aec9ab22f8446e015
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2025-01-29 19:19:55 +0900

    [Init] : 프로젝트 생성

---
+++ src/main/kotlin/com/example/mycinema/MycinemaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mycinema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MycinemaApplication
+
+fun main(args: Array<String>) {
+    runApplication<MycinemaApplication>(*args)
+}
